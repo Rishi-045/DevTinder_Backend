e17eeec3fb7678ca6d256a903324c53e5438a322
Rishikesh Pal <palrishikesh2004@gmail.com>
2026-01-27 23:25:52 +0530

    implement authentication using jwt

--- src/index.js
+++ src/index.js
@@ -3,116 +3,19 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 const bcrypt = require("bcrypt");
-//this is use to read environment variables
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+const authMiddleware = require("./middlewares/authMiddleware")
 require("dotenv").config();
-
-//this is the built-in middleware that parse the json into javascript object
 app.use(express.json());
+app.use(cookieParser());
 
-// get all the users from the database
-app.get("/feed", async (req, res) => {
-  try {
-    const user = await User.find();
-    if (user.length == 0) {
-      return res.status(404).json({ message: "Users not found" });
-    }
-
-    return res.json({ data: user });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      message: "Internal Server Error",
-    });
-  }
-});
-
-//update user by id
-app.patch("/user/:userId", async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const ALLOWED_UPDATES = [
-      "firstName",
-      "lastName",
-      "age",
-      "about",
-      "skills",
-      "photoUrl",
-    ];
-    const updates = Object.keys(req.body);
-    const isValidOperation = updates.every((update) =>
-      ALLOWED_UPDATES.includes(update),
-    );
-    if (!isValidOperation) {
-      return res.status(400).json({
-        message: "Invalid Updates",
-      });
-    }
-    const updatedData = await User.findByIdAndUpdate(userId, req.body, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    if (!updatedData) {
-      res.status(404).send("User not found.");
-    } else {
-      res.send({
-        message: "User data updated Successfully",
-        data: updatedData,
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      message: "Internal Server Error",
-      error: err.message,
-    });
-  }
-});
-
-//delete user by id
-app.delete("/user", async (req, res) => {
-  try {
-    const { id } = req.query;
-    const data = await User.findByIdAndDelete(id);
-    if (!data) {
-      res.status(404).send({
-        message: "User not Found",
-      });
-    } else {
-      res.send({
-        message: "User deleted Successfully.",
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      message: "Internal Server Error",
-    });
-  }
-});
-
-// get user by id
-app.get("/user", async (req, res) => {
-  try {
-    const { id } = req.query;
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    return res.json({ data: user });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      message: "Internal Server Error",
-    });
-  }
-});
 
 app.post("/signup", async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
     const passwordHash = await bcrypt.hash(password, 10);
-    console.log(passwordHash);
     const user = new User({
       firstName,
       lastName,
@@ -130,35 +33,47 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/profile", authMiddleware, async (req, res) => {
+  try {
+    const {user} = req;
+    return res.send({
+      data: user,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+});
+
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
       return res.status(400).json({
-        message: "Email and password are required"
+        message: "Email and password are required",
       });
     }
 
     //check in the db user exist or not
     const isUserExist = await User.findOne({ email: email });
     if (!isUserExist) {
-      return res
-        .status(401)
-        .json({
-          message: "Invalid Credentials"
-        });
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
     }
     const { password: encryptedPassword } = isUserExist;
     const isPasswordValid = await bcrypt.compare(password, encryptedPassword);
     if (!isPasswordValid) {
-      return res
-        .status(401)
-        .json({
-          message: "Invalid Credentials"
-        });
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
     }
-
+    const token = jwt.sign({ id: isUserExist._id }, process.env.SIGNATURE,{expiresIn: "1d"});
+    res.cookie("token", token,{expires: new Date(Date.now() + 24 * 60 * 60 * 1000),httpOnly: true});
     return res.status(200).json({
       message: "Logged in successfully",
     });
---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+const User = require("../models/user");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({
+        message: "Token not found",
+      });
+    }
+    const istokenValid = validator.isJWT(token);
+    if (!istokenValid) {
+      return res.status(401).json({
+        message: "Invalid Token",
+      });
+    }
+    const decodedData = jwt.verify(token, process.env.SIGNATURE);
+    const user = await User.findById(decodedData.id);
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
+
+module.exports = authMiddleware;
